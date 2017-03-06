3946b65ef547a46c8d03b728c5d2999419fe105d
kone.online <kone.online@yahoo.de>
2017-03-06 16:27:47 -0500

    update state to es7 syntax

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
+  state = {
       entries: [],
       currentEntry:"",
       edit: null,
       searchBackup: [],
       counter: 0
-    }
   }
   updateCurrentEntry = (e) => {
     this.setState({
